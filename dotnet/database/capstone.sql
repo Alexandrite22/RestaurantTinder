462c02c213af3019e24ab70eb59e7df1096f59d5
Nick Gotsy <nick.gotsy@gmail.com>
2022-12-07 09:47:11 -0500

    added sql tables

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -24,6 +24,45 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 )
 
+CREATE TABLE party (
+	party_id int IDENTITY(1,1) NOT NULL,
+	location varchar(50) NOT NULL,
+	date int NOT NULL,
+	owner varchar(50) NOT NULL,
+	description varchar(50),
+	name_of_party varchar(50) NOT NULL
+)
+
+CREATE TABLE restaurant (
+	restaurant_id int IDENTITY (1,1) NOT NULL,
+	party_id int IDENTITY (1,1) NOT NULL,
+	name varchar(50) NOT NULL,
+	Api_address varchar(200) NOT NULL,
+	CONSTRAINT fk_
+
+)
+
+CREATE TABLE guest (
+	guest_id int IDENTITY (1,1) NOT NULL,
+	name varchar(50) NOT NULL,
+	party_id int IDENTITY (1,1) NOT NULL,
+)
+
+CREATE TABLE liked_disliked (
+	like_disliked_id int IDENTITY (1,1) NOT NULL,
+	guest_id int IDENTITY (1,1) NOT NULL,
+	restaurant_id int IDENTITY (1,1) NOT NULL,
+	like_or_dislike varchar(10) NOT NULL
+)
+
+--TODOs: 
+--> link party w/ user table 
+--> party w/ guests
+--> party w/ restaurant
+--> guest w/ like_dislike
+--> restaurant w/ like_dislike
+
+
 --populate default data
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
