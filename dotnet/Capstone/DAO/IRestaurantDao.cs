b9f1ab6c2e2925fd2129a0b3f00c725d146ea9d7
Kevin Nye <kevin.nye@gmail.com>
2022-12-07 14:49:06 -0500

    some fixes on DAOs

--- dotnet/Capstone/DAO/IRestaurantDao.cs
+++ dotnet/Capstone/DAO/IRestaurantDao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,7 @@ namespace Capstone.DAO
 {
     public class IRestaurantDao
     {
+        //Restaurant GetRestaurant(string username);
+       // User AddUser(string username, string password, string role);
     }
 }
