a2232dcb6052744007414c8cc915dcb353fe221f
Colin Detwiller <colindetwillerjr@gmail.com>
2022-12-09 10:53:22 -0500

    Fixed backend getting list of all parties, and listing parties on frontend

--- vue/src/components/PartyList.vue
+++ vue/src/components/PartyList.vue
@@ -1,13 +1,32 @@
 <template>
     <div class="border">
-        <div v-for="party in parties" v-bind:key="party">
-            {{party}}
-        </div>
+        <h1>Party List</h1>
+        <h3>TODO: Finish this component</h3>
+        <!-- <b-table-lite striped hover :parties="parties"></b-table-lite> TODO: Figure out bootstrap-vue table --> 
+
+        <table >
+            <tr>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Location</th>
+                <!--<th>RSVPs</th> should we have a count of guests as rsvp -->
+            </tr>
+            <tr v-for="party in parties" v-bind:key="party.Id">
+                <td>{{party.Name}}</td>
+                <td>{{party.Date}}</td>
+                <td>{{party.Time}}</td>
+                <td>{{party.Location}}</td>
+                
+                <!--<td>{{party.partyRsvp}}</td> should we have a count of guests as rsvp -->
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
 import PartyService from '../services/PartyService.js';
+
 export default {
     name: 'party-list',
     data() {
@@ -22,7 +41,11 @@ export default {
         getParties() {
             PartyService.getParties(1)
                 .then(response => {
-                    this.parties = response.data;
+                    response.data.forEach(thing => {
+                        let temp = {  Id: thing.partyId, Name: thing.name, Date: thing.date, Time: thing.Date, Location: thing.location };
+                        this.parties.push(temp);
+                        console.log(temp);
+                    });
                 })
                 .catch(error => {
                     console.log(error);
