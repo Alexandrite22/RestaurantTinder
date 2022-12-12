6e347d6b90dbf6e46359e9c31ed578a4ff4fb664
Kevin Nye <kevin.nye@gmail.com>
2022-12-12 09:39:34 -0500

    tweaked scrollbar on dashboard

--- vue/src/components/MenuColumn.vue
+++ vue/src/components/MenuColumn.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="border">
-        <h1>Menu Column</h1>
-        <h6><router-link to="/">Home</router-link></h6>
-        <h6><a href="http://localhost:8080/newParty">Create party</a></h6>
+  <div class="border">
+    <h1>Menu</h1>
+    <h6><router-link to="/">Home</router-link></h6>
+    <h6><a href="http://localhost:8080/newParty">Create party</a></h6>
 
-        <h6><router-link to="/dashboard">View parties</router-link></h6>
-        <br/>
-        <br/>        
-        <h6><a href="http://localhost:8080/dashboard">My current plans</a></h6>
+    <h6><router-link to="/dashboard">View parties</router-link></h6>
+    <br />
+    <br />
+    <h6><a href="http://localhost:8080/dashboard">My current plans</a></h6>
 
-        <div v-if="this.$store.parties">
-            <ul>
-                <li v-for="party in this.$store.parties" v-bind:key="party.Id">
-                    <router-link :to="party.InviteLink">{{party.Name}}</router-link>
-                </li>
-            </ul>   
-        </div>
-        <!-- <ul>
+    <div v-if="this.$store.parties">
+      <ul>
+        <li v-for="party in this.$store.parties" v-bind:key="party.Id">
+          <router-link :to="party.InviteLink">{{ party.Name }}</router-link>
+        </li>
+      </ul>
+    </div>
+    <!-- <ul>
             <li>
                 Userette Nameabelle
             </li>
@@ -33,25 +33,24 @@
             
             </li>
         </ul>     -->
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
- name: 'MenuColumn',    
-    data() {
-        return {};
-    },
-    created() {
-        console.log("MenuColumn created");
-    },
-}
+  name: "MenuColumn",
+  data() {
+    return {};
+  },
+  created() {
+    console.log("MenuColumn created");
+  },
+};
 </script>
 
 <style>
-.border
-{
-    color:black;
-    opacity: 100%;
+.border {
+  color: black;
+  opacity: 100%;
 }
 </style>
