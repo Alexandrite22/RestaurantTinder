436c2e9e18b12f43fe9cc3e43f1a0f68c202dab1
Kevin Nye <kevin.nye@gmail.com>
2022-12-12 09:00:05 -0500

    a couple of cosmetic changes on home page

--- vue/src/components/DetailsColumn.vue
+++ vue/src/components/DetailsColumn.vue
@@ -1,18 +1,20 @@
 <template>
-    <div class="details-column border">Hello I hate frontend</div>
+  <div class="details-column border">
+    Still determining how to display the proper information in this column
+  </div>
 </template>
 
 <script>
 export default {
-    name: "DetailsColumn",
-    data() {
-        return {};
-    },
-}
+  name: "DetailsColumn",
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-  .details-column {
-    background-color: red;
-    }
+.details-column {
+  background-color: red;
+}
 </style>
--- vue/src/components/SiteDescription.vue
+++ vue/src/components/SiteDescription.vue
@@ -1,33 +1,27 @@
 <template>
+  <div>
     <div>
-        <div>
-            <b-col class="col-12">
-                ME-n-U
-            </b-col>
-        </div>
-        <div class="col-12">
-            <span>
-                Here at Me-n-U we aim to revolutionize how groups select restaurants for anything for dates, to large parties, to corporate events! Use our service to let everyone decide which restaurant will let your event be a smash hit!
-            </span>
-        </div>
-        <div class="row">
-            <v-btn class="btn btn-primary col-3" >
-                LOGIN
-            </v-btn>
-            <div class=" col-2"></div>
-            <v-btn class="btn btn-secondary col-3" >
-                SIGN-UP
-            </v-btn>
-        </div>
+      <b-col class="col-12"> ME-n-U </b-col>
     </div>
+    <div class="col-12">
+      <span>
+        Here at Me-n-U we aim to revolutionize how groups select restaurants for
+        anything from dates to large parties to corporate events! Use our
+        service to let everyone decide which restaurant will let your event be a
+        smash hit!
+      </span>
+    </div>
+    <!-- <div class="row">
+      <v-btn class="btn btn-primary col-3"> LOGIN </v-btn>
+      <div class="col-2"></div>
+      <v-btn class="btn btn-secondary col-3"> SIGN-UP </v-btn>
+    </div> -->
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
