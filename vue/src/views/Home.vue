4cd046bc4239cf6e09920fcb2a77eee408db0d67
Kevin Nye <kevin.nye@gmail.com>
2022-12-15 18:12:07 -0500

    fixed the menu links and the deadspace in the center column

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,26 +2,24 @@
   <div class="home">
     <h1><center>Home</center></h1>
     <p>
-        Here at Me-n-U we aim to revolutionize how groups select restaurants for
-        anything from dates to large parties to corporate events! Use our
-        service to let everyone decide which restaurant will let your event be a
-        smash hit!
+      Here at Me-n-U we aim to revolutionize how groups select restaurants for
+      anything from dates to large parties to corporate events! Use our service
+      to let everyone decide which restaurant will let your event be a smash
+      hit!
     </p>
-    <button @click="TestMethod()">
-
-    </button>
+    <!-- <button @click="TestMethod()"></button> -->
   </div>
 </template>
 
 <script>
-import YelpService from "../services/YelpService.js";
-export default {
-  methods: {
-    TestMethod() {
-      YelpService.getRestaurants();
-    },
-  },
-};
+// import YelpService from "../services/YelpService.js";
+// export default {
+//   methods: {
+//     TestMethod() {
+//       YelpService.getRestaurants();
+//     },
+//   },
+// };
 </script>
 <style >
 /* .menuColumn {
